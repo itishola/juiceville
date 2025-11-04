6a9615029598c0ea1e410608ef4bcb4d18f8b1f4
Issa <issa@juicevillecakesandtreats@gmail.com>
2025-11-04 07:11:41 +0100

    Prepare for PythonAnywhere deployment

--- juiceville/settings.py
+++ juiceville/settings.py
@@ -17,7 +17,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -50,12 +49,10 @@ INSTALLED_APPS = [
 
     'crispy_forms',
     'crispy_bootstrap4',
-      
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google', 
-
     'pwa',
 ]
 
